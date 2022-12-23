f75cca42ff1a9f4398fc7d4b29c30a9d309c2d74
roller0201 <119750860+roller0201@users.noreply.github.com>
2022-12-23 12:54:28 +0100

    23.12

---
+++ Complete Course from zero to expert/script.js
@@ -0,0 +1,44 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+let firstName = "Jonas";
+console.log(firstName);*/
+
+/*true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "String");
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);*/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990; // type error
+var job = "programmer";
+job = "teacher";*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageSarah, ageJonas);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
