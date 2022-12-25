b52a9d8fe963d4f0456c2204973a20d4a54e880c
roller0201 <119750860+roller0201@users.noreply.github.com>
2022-12-25 14:05:11 +0100

    Update script.js

--- Complete Course from zero to expert/script.js
+++ Complete Course from zero to expert/script.js
@@ -30,7 +30,7 @@ const birthYear = 1991;
 var job = "programmer";
 job = "teacher";*/
 // Math operation
-const now = 2037;
+/*const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageSarah, ageJonas);
@@ -57,4 +57,31 @@ console.log(now - 1991 > now - 2019);
 console.log(25 - 10 - 5);
 let z, y;
 z = y = 25 - 10 - 5;
-console.log(z, y);
+console.log(z, y);*/
+
+let marksMass = 78;
+let marksHeights = 1.69;
+let johnsMass = 92;
+let johnsHeights = 1.95;
+
+let bodyMassIndexmarks = marksMass / marksHeights ** 2;
+console.log(bodyMassIndexmarks);
+
+let bodyMaxIndexJohns = johnsMass / johnsHeights ** 2;
+console.log(bodyMaxIndexJohns);
+
+let marksMass2 = 95;
+let marksHeights2 = 1.88;
+let johnsMass2 = 85;
+let johnsHeights2 = 1.76;
+
+let bodyMassIndexmarks2 = marksMass2 / marksHeights2 ** 2;
+console.log(bodyMassIndexmarks2);
+
+let bodyMaxIndexJohns2 = johnsMass2 / johnsHeights2 ** 2;
+console.log(bodyMaxIndexJohns2);
+
+markHigherBMI = bodyMassIndexmarks > bodyMaxIndexJohns;
+console.log(markHigherBMI);
+markHigherBMI2 = bodyMassIndexmarks2 > bodyMaxIndexJohns2;
+console.log(markHigherBMI2);
